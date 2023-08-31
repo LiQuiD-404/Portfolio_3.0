038f032da65ada8cb425c642f172553b53296daa
Shubham Singh <shubhamsingh.exe@gmail.com>
2023-08-31 15:08:34 +0530

    Updated Code

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,32 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
 });
-
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
 ScrollTrigger.scrollerProxy("#main", {
   scrollTop(value) {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  },
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
   getBoundingClientRect() {
     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
   },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
 });
 
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
+
 
 function loader() {
     var t1 = gsap.timeline();
@@ -117,7 +125,6 @@ function animateHome() {
         onComplete: function(){
             gsap.set(".second svg", {opacity: 100 });
             animateSVG();
-            enableScroll();
         }
 
     })
@@ -126,39 +133,39 @@ function animateHome() {
 
 gsap.set(".intro .left .content", { y: "-200%", opacity: 0 });
 
-gsap.to(".pf",{
+gsap.to(".pf", {
     transform: "rotate(0) translate(0,0)",
     ease: "sine.in",
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: ".intro",
         scroller: "#main",
-        scrub:1,
+        scrub: 1,
         end: "top 10%"
     }
 })
-gsap.to(".intro .left .content",{
+gsap.to(".intro .left .content", {
     transform: "translate(0,0)",
     opacity: 1,
     ease: "sine.in",
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: ".intro",
         scroller: "#main",
-        scrub:1,
+        scrub: 1,
         end: "top 30%"
     }
 })
 
 gsap.set(".skillset", { x: "-200%", opacity: 0, transform: "rotate(-10deg)", });
 
-gsap.to(".skillset",{
+gsap.to(".skillset", {
     x: 0,
     opacity: 1,
     stagger: 0.2,
     delay: 1,
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: ".skills_main",
         scroller: "#main",
-        scrub:1,
+        scrub: 1,
         end: "top 1%"
     }
 })
@@ -183,6 +190,31 @@ document.querySelectorAll(".img2").forEach(function (elem) {
         document.querySelector(".works").style.backgroundColor = "#f2f2f2";
     })
 
+})
+
+document.getElementById('overcast').addEventListener('click', function () {
+    window.open("https://liquid-404.github.io/OverCast-Weather-App/", "_blank")
+})
+
+document.getElementById('lumos').addEventListener('click', function () {
+    window.open("https://lumos-gradients.vercel.app/", "_blank")
+})
+
+document.getElementById('portfolio').addEventListener('click', function () {
+    window.open("https://shubbhh.vercel.app/", "_blank")
+})
+
+gsap.set(".social_container .card", { y: "-100%", opacity: 0, transform: "rotate(150deg)" });
+
+gsap.to(".social_container .card", {
+    y: 0,
+    transform: "rotate(0deg)",
+    opacity: 1,
+    stagger: 0.3,
+    scrollTrigger:{
+        trigger: ".social_container",
+        scroller: "#main"
+    }
 })
 
 loader();
